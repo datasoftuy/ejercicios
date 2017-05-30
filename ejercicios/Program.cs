a7166c34e717878ef272b6d80903a682be0a4293
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-30 15:00:53 -0300

    terminado

--- ejercicios/Program.cs
+++ ejercicios/Program.cs
@@ -11,10 +11,50 @@ namespace leapyear
         {
 
             bool salir = false;
-            string otravez;
+            string otravez,opcion = "";
+            int[] numeros = new int[51];
+            
+
+
             while (salir != true)
             {
-                Console.WriteLine("OK    while    ");
+                Console.Clear();
+                Console.WriteLine("1 Cargar los vectores");
+                Console.WriteLine("2 Mostrar de forma decendente");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+
+                    case "1":
+
+                            Console.WriteLine("Se carga el vector numeros del 1 al 50");
+                           
+                            for (int i = 0;  i  <  51  ; i++){
+
+                            numeros[i] = i;
+                    
+                            }
+
+
+                        break;
+
+
+                    case"2":
+
+                        Console.WriteLine("Mostrar en orden decendente");
+
+
+                            for (int i = 50; i >= 0; i--){
+                
+                             Console.WriteLine("" + numeros[i]);
+                            
+                             }
+                            Console.ReadLine();
+                            
+                            break;
+
+                }
+
 
                 Console.WriteLine("Presione s para salir");
                 otravez = Console.ReadLine().ToLower();
@@ -25,7 +65,6 @@ namespace leapyear
                 
                 
                 }
-
 
             }
               
