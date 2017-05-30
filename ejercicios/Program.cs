9593c94637a6fc79928215304c4ab623ee7e7081
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-30 11:16:09 -0300

    while  mal

---
+++ ejercicios/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string salir;
+            bool seguir = true;
+
+
+            while (seguir != false)
+
+
+
+
+            {
+
+                Console.WriteLine("OK");
+            
+            
+            
+            }
+
+            Console.WriteLine("Presione s para salir");
+            salir = Console.ReadLine().ToUpper();
+
+            if (salir == "s")
+            {
+                seguir = false;
+
+
+
+
+
+            }
+        
+        
+        }
+    }
+}
